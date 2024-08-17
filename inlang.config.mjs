f726941586880e9f1ca824230d3f5907c1790b9a
Hrihorii Ilin <devopsick@pm.me>
2024-08-18 00:03:47 +0300

    first commit

---
+++ inlang.config.mjs
@@ -0,0 +1,11 @@
+export const plugins = {
+  i18next: {
+    translationFilePattern: './src/locales/{{locale}}.json',
+  },
+};
+export const locales = ['en', 'fr', 'de', 'es'];
+export const sourceLocale = 'en';
+export const messageFilePattern = './src/locales/{locale}.json';
+export const languages = ["en", "fr", "es", 'de'];
+export const sourceLanguage = "en";
+export const autoTranslate = true;
